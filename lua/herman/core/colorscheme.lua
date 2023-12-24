837f530d33c6989f5f972b0d25d9957f175544e3
German Berdnikov <applejay1802@gmail.com>
2023-12-23 22:30:36 -0500

    update

--- lua/herman/core/colorscheme.lua
+++ lua/herman/core/colorscheme.lua
@@ -1,5 +1,12 @@
 -- set colorscheme to nightfly with protected call
 -- in case it isn't installed
+require("tokyonight").setup({
+	transparent = true,
+	styles = {
+		sidebars = "transparent",
+		floats = "transparent",
+	},
+})
 local status, _ = pcall(vim.cmd, "colorscheme tokyonight-night")
 if not status then
 	print("Colorscheme not found!") -- print error if colorscheme not installed
